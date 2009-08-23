62a852c82b1f3dfccfb124736d513c236667b6aa
Matt Darby <matt@matt-darby.com>
2009-08-23 11:48:02 -0400

    .

--- install.rb
+++ install.rb
@@ -7,6 +7,6 @@ FileUtils.cp_r(src_file, dest_file)
 
 # Images
 FileUtils.mkdir "#{RAILS_ROOT}/public/images/socialite"
-FileUtils.cp_r "#{FILE.dirname(__FILE__)}/assets/images/", "#{RAILS_ROOT}/public/images/socialite"
+FileUtils.cp_r "#{FILE.dirname(__FILE__)}/assets/images/*", "#{RAILS_ROOT}/public/images/socialite/"
 
 puts "Files copied - Installation complete!" 
