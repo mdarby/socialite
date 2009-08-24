edc61759976d173e4520037bcbbcbcb14da43911
Matt Darby <matt@matt-darby.com>
2009-08-23 23:38:43 -0400

    Cleaned and tightened

--- lib/socialite.rb
+++ lib/socialite.rb
@@ -1,13 +1,13 @@
 module Socialite
-  
+
   def socialite(options)
     url   = options[:url]
     title = options[:title]
-    
+
     unless url.present? && title.present?
       RAILS_DEFAULT_LOGGER.warn "Socialite requires a URL and title!"
-    else    
-      links = { 
+    else
+      links = {
         :delicious   => "http://www.delicious.com/post?url=#{url}&title=#{title}",
         :digg        => "http://www.digg.com/submit?url=#{url}&title=#{title}",
         :facebook    => "http://www.facebook.com/share.php?u=#{url}",
@@ -16,10 +16,10 @@ module Socialite
         :technorati  => "http://www.technorati.com/faves?add=#{url}&title=#{title}",
         :twitter     => "http://www.twitter.com/?status=#{title}: #{url}",
         :email       => "mailto:?subject=#{title}&body=#{title}: #{url}"
-      }.inject("") do |stack, (site, submit_url)|
-        stack << "<a href='#{submit_url}'><img src='/images/socialite/#{site}_16.png'></a>"
+      }.map do |(site, submit_url)|
+        "<a href='#{submit_url}'><img src='/images/socialite/#{site}_16.png'></a>"
       end
-    
+
       <<-END
         <div id="socialite">
           <span class="advert">
@@ -27,13 +27,13 @@ module Socialite
               Socialite
             </a>
           </span>
-        
+
           <div id="links">
             #{links}
           </div>
         </div>
       END
     end
-  
+
   end
 end
